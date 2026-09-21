8e349b6e6ed931a3e38e62bd8cd9b70603e9312e
MD Kamre Alam <kamre8269@gmail.com>
2026-09-21 20:50:22 +0530

    chore: configure Tailwind CSS with Vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <main>
-      <h1>Afroz Digital Seva</h1>
+    <main className="grid min-h-screen place-content-center bg-slate-50 px-5 text-center">
+      <h1 className="mb-3 text-4xl font-bold text-green-700">
+        Afroz Digital Seva
+      </h1>
 
-      <p>
+      <p className="text-lg  text-amber-300 ">
         All Government and Online Services Under One Roof
       </p>
     </main>
